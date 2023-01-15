da048337ea042f94379ec884d42ec82cfba1c07b
emin <eminyetim25@gmail.com>
2023-01-16 00:44:25 +0300

    first

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace dictimory
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("Dictinory");
+
+            //Dictinory <key , value> dicKullanicilar = new Dictinory <key , string >();
+
+            Dictionary<int , string> kullanicilar = new Dictionary<int, string>();
+            kullanicilar.Add(1001,"Ahmet Yilmaz");
+            kullanicilar.Add(1002,"Ali Yilmaz");
+            kullanicilar.Add(1003,"Veli Yilmaz");
+
+        }
+    }
+}
